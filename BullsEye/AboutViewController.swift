d3e2f6cc95f35a99e79e4af0ecfb0fafee02fabe
sandra <sandra.h.arriaga@gmail.com>
2019-11-14 21:56:43 -0600

    Add info viewController

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Sandra Herrera on 11/14/19.
+//  Copyright © 2019 Sandra Herrera. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -27,9 +27,11 @@ class ViewController: UIViewController {
     
     var targetValue = 0
     var score = 0
+    var round = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         let roundedValue = slider.value.rounded()
+        roundValue.text = String(round)
         currentValue = Int(roundedValue)
         changeTargetValue()
     }
@@ -42,25 +44,29 @@ class ViewController: UIViewController {
     }
     
     
-    func showAlert(points : Int)
+    func showAlert(points : Int, difference : Int)
     {
+        
+        
         let message = "Your score: \(points)"
-        let alertVC = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
+        let alertVC = UIAlertController(title: titleAlert(difference: difference), message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "Ok", style: .default, handler:  {alert in
             //_ in trailling clousure
             
-            self.score += points
-            self.scoreLabel.text = String(self.score)
-            
+            //self.score += points
+            //self.scoreLabel.text = String(self.score)
+            //self.roundValue.text = String(self.round)
+            self.startNewRound()
         })
         alertVC.addAction(action)
         present(alertVC,animated: true, completion: nil)
         
-        startNewRound()
+        
     
     }
 
     func startNewRound(){
+        round += 1
         targetValue = Int.random(in: 1...100)
         currentValue = 50
         slider.value = Float(currentValue)
@@ -70,27 +76,57 @@ class ViewController: UIViewController {
     func updateLabels()
     {
         targetLabel.text = String(targetValue)
+        self.roundValue.text = String(self.round)
+    }
+    func titleAlert(difference : Int) -> String {
+     
+        if difference == 0 {
+            return "Perfect"
+        } else if difference < 5 {
+            return "You almost had it"
+        } else if difference < 10 {
+            return "Pretty Good"
+        }
+        else {
+            return "Not even close."
+        }
+       
     }
     
     @IBAction func sliderChanged(_ slider: UISlider)
     {
+        scoreLabel.text = String(score)
         currentValue = Int(slider.value.rounded())
+        roundValue.text = String(round)
         print("\(slider.value)")
     }
     
     @IBAction func hitMeTappead(_ sender: Any)
     {
         let difference = abs(targetValue - currentValue)
-        let points  = 100 - difference
-    
+        var points  = 100 - difference
+        if difference == 0 {
+            points += 100
+        } else if difference == 5{
+            points += 50
+        }
+        self.score += points
+        self.scoreLabel.text = String(self.score)
+        self.roundValue.text = String(self.round)
         
-        showAlert(points: points)
+        showAlert(points: points, difference: difference)
     }
     
     
     @IBAction func resetButton(_ sender: Any)
     {
-        
+        newGame()
+    }
+    
+    func newGame(){
+        score = 0
+        round = 0
+        startNewRound()
     }
 }
 
